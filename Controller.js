864ba40ea52f4c37489412bc3e9d5a0d3b838ca4
git-moss <juergen.mossgraber@googlemail.com>
2015-09-18 22:06:23 +0200

    Fixed setting maximum value.

--- Controller.js
+++ Controller.js
@@ -87,7 +87,7 @@ function changeValue (control, value, fractionValue, maxParameterValue, minParam
         minParameterValue = 0;
     var isInc = control >= 65;
     var speed = Math.max ((isInc ? control - 65 : Math.abs (63 - control)) * fractionValue, fractionValue);
-    return isInc ? Math.min (value + speed, maxParameterValue) : Math.max (value - speed, minParameterValue);
+    return isInc ? Math.min (value + speed, maxParameterValue - 1) : Math.max (value - speed, minParameterValue);
 }
 
 Scales.DRUM_NOTE_END = 52;
