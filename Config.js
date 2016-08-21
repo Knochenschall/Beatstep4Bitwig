7662046422cfc2c38b1441eda8d1b70392271380
git-moss <juergen.mossgraber@googlemail.com>
2016-08-21 08:31:55 +0200

    Framework update.

--- Config.js
+++ Config.js
@@ -1,6 +1,8 @@
 // Written by Jürgen Moßgraber - mossgrabers.de
-// (c) 2014-2015
+// (c) 2014-2016
 // Licensed under LGPLv3 - http://www.gnu.org/licenses/lgpl-3.0.txt
+
+load ("framework/core/AbstractConfig.js");
 
 // ------------------------------
 // Static configurations
@@ -20,10 +22,7 @@ Config.SCALES_BASE   = 1;
 Config.SCALES_IN_KEY = 2;
 Config.SCALES_LAYOUT = 3;
 
-Config.scale       = 'Major';
-Config.scaleBase   = 'C';
-Config.scaleInKey  = true;
-Config.scaleLayout = '4th ^';
+Config.initListeners (Config.SCALES_LAYOUT);
 
 Config.init = function ()
 {
@@ -32,74 +31,8 @@ Config.init = function ()
     ///////////////////////////
     // Scale
 
-    var scaleNames = Scales.getNames ();
-    Config.scaleSetting = prefs.getEnumSetting ("Scale", "Scales", scaleNames, scaleNames[0]);
-    Config.scaleSetting.addValueObserver (function (value)
-    {
-        Config.scale = value;
-        Config.notifyListeners (Config.SCALES_SCALE);
-    });
-    
-    Config.scaleBaseSetting = prefs.getEnumSetting ("Base", "Scales", Scales.BASES, Scales.BASES[0]);
-    Config.scaleBaseSetting.addValueObserver (function (value)
-    {
-        Config.scaleBase = value;
-        Config.notifyListeners (Config.SCALES_BASE);
-    });
-
-    Config.scaleInScaleSetting = prefs.getEnumSetting ("In Key", "Scales", [ "In Key", "Chromatic" ], "In Key");
-    Config.scaleInScaleSetting.addValueObserver (function (value)
-    {
-        Config.scaleInKey = value == "In Key";
-        Config.notifyListeners (Config.SCALES_IN_KEY);
-    });
-
-    Config.scaleLayoutSetting = prefs.getEnumSetting ("Layout", "Scales", Scales.LAYOUT_NAMES, Scales.LAYOUT_NAMES[0]);
-    Config.scaleLayoutSetting.addValueObserver (function (value)
-    {
-        Config.scaleLayout = value;
-        Config.notifyListeners (Config.SCALES_LAYOUT);
-    });
+    Config.activateScaleSetting (prefs);
+    Config.activateScaleBaseSetting (prefs);
+    Config.activateScaleInScaleSetting (prefs);
+    Config.activateScaleLayoutSetting (prefs);
 };
-
-Config.setScale = function (scale)
-{
-    Config.scaleSetting.set (scale);
-};
-
-Config.setScaleBase = function (scaleBase)
-{
-    Config.scaleBaseSetting.set (scaleBase);
-};
-
-Config.setScaleInScale = function (inScale)
-{
-    Config.scaleInScaleSetting.set (inScale ? "In Key" : "Chromatic");
-};
-
-Config.setScaleLayout = function (scaleLayout)
-{
-    Config.scaleLayoutSetting.set (scaleLayout);
-};
-
-// ------------------------------
-// Property listeners
-// ------------------------------
-
-Config.listeners = [];
-for (var i = 0; i <= Config.SCALES_LAYOUT; i++)
-    Config.listeners[i] = [];
-
-Config.addPropertyListener = function (property, listener)
-{
-    Config.listeners[property].push (listener);
-};
-
-Config.notifyListeners = function (property)
-{
-    var ls = Config.listeners[property];
-    for (var i = 0; i < ls.length; i++)
-        ls[i].call (null);
-};
-
-function Config () {}
